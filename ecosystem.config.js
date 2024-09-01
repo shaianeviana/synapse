09f0a9c43fc6eca535d26c358c01f4451fb919f2
Augustos0204 <course.gabrielaug@gmail.com>
2024-09-01 08:31:36 -0300

    updated the options for the ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,11 @@ module.exports = {
     {
       name: 'synapse',
       script: './node_modules/next/dist/bin/next',
-      args: "start -p 3000"
+      args: "start -p 3000",
+      instances: 2,
+      watch: false,
+      autorestart: true,
+      exec_mode: "cluster"
     }
   ],
 };
