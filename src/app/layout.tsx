4092e510cd3d093b4a87f1d7ded7d5fc1be92d29
Yan Victor <yanvictorsn@gmail.com>
2024-09-22 11:20:26 -0300

    feature:wallet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
-import LogoImage from "@/assets/synapse.png";
-
+import ConnectHeader from "@/componet/ConnectHeader";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Synapse",
   description: "A simple interface to make swaps with Monad",
 };
+
 
 export default function RootLayout({
   children,
@@ -27,22 +26,13 @@ export default function RootLayout({
         "to-pink-100",
         "flex flex-col flex-grow items-center"
         )}>
-          <header className={cn(
-            "w-[1200px] h-16 px-10",
-            "flex items-center space-x-4",
-          )}>
-            <Image src={LogoImage} alt="Synapse Logo" width={60} height={50}/>
-            <ul className="flex flex-row space-x-4 select-none cursor-pointer">
-              <li className="text-primary-custom hover:text-secondary-custom">Swap</li>
-              <li className="text-primary-custom hover:text-secondary-custom">Pool</li>
-              <li className="text-primary-custom hover:text-secondary-custom">About</li>
-              <li className="text-primary-custom hover:text-secondary-custom">Partners</li>
-            </ul>
-          </header>
-          <div className={cn(
-            "w-full flex-grow",
-            "flex flex-col justify-center items-center"
-          )}>
+        <ConnectHeader />
+          <div
+            className={cn(
+              "w-full flex-grow",
+              "flex flex-col justify-center items-center"
+            )}
+          >
             {children}
           </div>
         </body>
